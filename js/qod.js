4e02fb47e1b6b9e4f582e65308b0f6685f74c8ae
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-19 13:27:49 -0800

    Init

---
+++ js/qod.js
@@ -0,0 +1,15 @@
+(function($){
+    $('body').append('Hello World');
+    // 1. get request to grab random post and append to the DOM
+
+    // add a click event for the get a new post on the "show me another" button and run the ajax below
+    $.ajax({
+        method: "GET",
+        // url: // API Vars.rest_url +  wp-json/wp/v2/posts?filter[orderby]=rand$filter[posts_per_page]=1
+    }).done(function(data){
+        console.log(data);
+    }).fail(function(error){
+        console.log("An Error has occured!", error) 
+    })
+    // 2. posta new quote using the post method
+})(jQuery)
