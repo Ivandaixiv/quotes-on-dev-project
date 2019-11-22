a3f189d84d9e420688969b4e774b875d57eb18c4
Ivan Dai <ivandaixivwork@gmail.com>
2019-11-22 11:56:55 -0800

    Javascript for new quote button completed, adjustment to the functions.php file

--- js/qod.js
+++ js/qod.js
@@ -14,9 +14,20 @@
             }).done(function(data){
 
                 const post = data[0];
+                console.log(post);
                 history.pushState(null,null, qod_vars.home_url + '/' + post.slug);
                 // 1st value is object to manage state 2nd value is url title for browser tab
-                
+                $('.entry-content p').html(post.content.rendered);
+                $('.entry-title').html(post.title.rendered);
+                if(post._qod_quote_source_url == ''){
+                    if(post._qod_quote_source != '') {
+                        $('.source').html(' ,' + post._qod_quote_source);
+                    } else {
+                        $('.source').html('');
+                    }
+                } else {
+                    $('.source').html(', <a href="' + post._qod_quote_source_url + '">' + post._qod_quote_source + '</a>')
+                }
                 // window.location.replace(data[0].link);
             }).fail(function(error){
                 console.log("An Error has occured!", error);
